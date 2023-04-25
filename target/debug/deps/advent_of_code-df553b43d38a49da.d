2a763396b835f71322400eeef6d5b440f07e8fa3
Mohamed Osama <mohammedosama.amagd@gmail.com>
2023-04-26 01:05:59 +0200

    Change Project Structure

---
+++ target/debug/deps/advent_of_code-df553b43d38a49da.d
@@ -0,0 +1,11 @@
+/Users/mohamedosama/Documents/Dev/advent_of_code_2022/target/debug/deps/advent_of_code-df553b43d38a49da: src/main.rs src/day1/mod.rs src/day1/day1a.rs src/day1/day1b.rs src/day2/mod.rs src/day2/day2a.rs src/day2/day2b.rs
+
+/Users/mohamedosama/Documents/Dev/advent_of_code_2022/target/debug/deps/advent_of_code-df553b43d38a49da.d: src/main.rs src/day1/mod.rs src/day1/day1a.rs src/day1/day1b.rs src/day2/mod.rs src/day2/day2a.rs src/day2/day2b.rs
+
+src/main.rs:
+src/day1/mod.rs:
+src/day1/day1a.rs:
+src/day1/day1b.rs:
+src/day2/mod.rs:
+src/day2/day2a.rs:
+src/day2/day2b.rs:
